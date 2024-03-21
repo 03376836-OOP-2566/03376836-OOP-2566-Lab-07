8e192637ad781519a6c424b906d1d58d201f5f3b
likunzz <65030028@kmitl.ac.th>
2024-03-21 11:44:57 +0700

    4

---
+++ 0028/Lab07_Ex04/Program.cs
@@ -0,0 +1,19 @@
+﻿MyClass mc = new MyClass();
+mc.MyValue = 50;
+System.Console.WriteLine($"My value  = {mc.MyValue}");
+
+class MyClass
+{
+    private int StoredValue;
+    public int MyValue
+    {
+        get
+        {
+            return StoredValue;
+        }
+        set
+        {
+            StoredValue = value;
+        }
+    }
+}
