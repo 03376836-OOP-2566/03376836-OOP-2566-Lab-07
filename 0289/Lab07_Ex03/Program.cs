1ebd49f5ef15a5df527c7d7f750dcf00d5eb8d0c
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 02:37:51 +0700

    03376836-OOP-2566-Lab-07

---
+++ 0289/Lab07_Ex03/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+MyClass mc = new MyClass();
+System.Console.WriteLine($"My value  = {mc.MyValue}");
+
+mc.MyValue = 50;
+System.Console.WriteLine($"My value  = {mc.MyValue}");
+
+class MyClass
+{
+    private const double My_PI =  Math.PI;
+    public double MyValue
+    {
+        get
+        {
+            return My_PI;
+        }
+    }
+}
---
+++ 0289/Lab07_Ex04/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+MyClass mc = new MyClass();
+mc.MyValue = 50;
+System.Console.WriteLine($"My value  = {mc.MyValue}");
+
+class MyClass
+{
+    private int StoredValue;
+    public int MyValue
+    {
+        set
+        {
+            StoredValue = value;
+        }
+    }
+}
