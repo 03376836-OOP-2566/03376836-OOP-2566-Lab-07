42087e1bf99e56eee368f6317712f04ea76a23f4
likunzz <65030028@kmitl.ac.th>
2024-03-21 11:41:18 +0700

    3

---
+++ 0028/Lab07_Ex03/Program.cs
@@ -0,0 +1,26 @@
+﻿MyClass mc = new MyClass();
+System.Console.WriteLine($"My value  = {mc.MyValue}");
+
+mc.MyValue = 50;
+System.Console.WriteLine($"My value  = {mc.MyValue}");
+
+class MyClass
+{
+    private double myValue; 
+
+    public MyClass()
+    {
+        myValue = Math.PI; 
+    }
+    public double MyValue
+    {
+        get
+        {
+            return myValue;
+        }
+        set
+        {
+            myValue = value;
+        }
+    }
+}
