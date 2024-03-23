65d00eb720dd96dab49633e31832c37ddca7625d
nitiphum <65030130@kmitl.ac.th>
2024-03-24 00:19:05 +0700

    commitex4

--- Lab07_Ex04/Program.cs
+++ Lab07_Ex04/Program.cs
@@ -1,12 +1,15 @@
 ﻿MyClass mc = new MyClass();
 mc.MyValue = 50;
-System.Console.WriteLine($"My value  = {mc.MyValue}");
-
+System.Console.WriteLine($"My value = {mc.MyValue}");
 class MyClass
 {
     private int StoredValue;
     public int MyValue
     {
+        get
+        {
+            return StoredValue;
+        }
         set
         {
             StoredValue = value;
